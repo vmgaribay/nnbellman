6b5dc98c16587be4de8cdb6769c06ab33fd9aba3
Victoria <victoria@Victorias-MacBook-Pro.local>
2024-11-01 17:35:39 +0100

    added additional scripts and configs for simultaneous runs on Snellius.

---
+++ nnbellman/as_both_grid_search_S42gpu.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#SBATCH --job-name=asNNS42
+#SBATCH -p gpu
+#SBATCH --gpus=1
+
+#              d-hh:mm:ss
+#SBATCH --time=12:30:00
+
+
+module load 2022
+#python -m venv NNFun 
+source NNFun/bin/activate
+#pip install -r requirements.txt
+
+#cp $HOME/NNFunction/Trial3/DatasetSmall/ResultsFinal.csv "$TMPDIR"
+#cp $HOME/NNFunction/Trial3/DatasetSmall/AgentData.csv "$TMPDIR"
+#mkdir $TMPDIR/saved/i_a
+
+
+# Experimental setup for seed 42
+
+architecture=("ThreeLayer" "FourLayer" "FiveLayer" "PudgeFiveLayer" "PudgeSixLayer")
+learningrate=("0.001" "0.01" "0.0001")
+batchsize=("64" "128" "512")
+maxnodes=("512" "1024" "2048")
+
+total_runs=$(( ${#architecture[@]} * ${#learningrate[@]} * ${#batchsize[@]} * ${#maxnodes[@]} ))
+counter=0
+restart=0
+earlystop=135
+
+
+# Loop through every combination
+for a in "${architecture[@]}"
+do
+    for lr in "${learningrate[@]}"
+    do
+        for bs in "${batchsize[@]}"
+        do
+            for mn in "${maxnodes[@]}"
+            do
+                ((counter++))
+                if [ "$counter" -ge "$restart" ] && [ "$counter" -le "$earlystop" ]; then
+                    name="both_${a}_${mn}"
+                    date=$(date)
+                    echo "$date Started run $counter/$total_runs with architecture: $a, learning rate: $lr, batch size: $bs, max nodes: $mn"
+                    variation="-c both_config_snell2_all_scaled.json --a $a --lr $lr --bs $bs --s 42 --mn $mn --n $name"
+                    python train.py $variation 
+                    date=$(date)
+                    echo "$date Finished run $counter/$total_runs with architecture: $a, learning rate: $lr, batch size: $bs, max nodes: $mn"
+                fi
+            done
+            
+        done
+    done
+done
+
+wait
+
+#cp -r "$TMPDIR"/saved $HOME/NNFunction/Trial3/saved
+
+echo " $date - Runs $restart through $earlystop are complete for seed 42."
+
